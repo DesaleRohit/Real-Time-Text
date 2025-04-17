7cf5914818a9c1af9d9615b5ee9be5ff617aa7cf
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-18 00:15:03 +0530

    [ADDED]:Rohit - Real time text

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function showText() {
     const contain = document.querySelector('.js-input');
     const show = contain.value;
-    console.log(show);
     document.querySelector('.js-result').innerHTML = show;
 }
